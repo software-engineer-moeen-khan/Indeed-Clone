890a4e39804b80ccd1e00816d852387b86d565e2
software-engineer-moeen-khan <121344844+software-engineer-moeen-khan@users.noreply.github.com>
2026-08-15 23:44:46 +0500

    Make login redirect flow database-safe

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -25,17 +25,9 @@ class AuthenticatedSessionController extends Controller
     public function store(LoginRequest $request): RedirectResponse
     {
         $request->authenticate();
-
         $request->session()->regenerate();
 
-        Auth::user()->update(['last_login_at' => now()]);
-
-        $intendedUrl = $request->input('intended_url');
-        if ($intendedUrl && $intendedUrl !== route('login')) {
-            return redirect()->to($intendedUrl);
-        }
-
-        return redirect()->route('dashboard');
+        return redirect()->intended(route('dashboard'));
     }
 
     /**
@@ -46,7 +38,6 @@ class AuthenticatedSessionController extends Controller
         Auth::guard('web')->logout();
 
         $request->session()->invalidate();
-
         $request->session()->regenerateToken();
 
         return redirect('/');
