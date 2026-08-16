4a138f1d8b4bfd7ac438c8f15c9baaa8e63f5b33
software-engineer-moeen-khan <121344844+software-engineer-moeen-khan@users.noreply.github.com>
2026-08-16 22:44:09 +0500

    Add popular search create page

---
+++ app/Filament/Resources/PopularSearchResource/Pages/CreatePopularSearch.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\PopularSearchResource\Pages;
+
+use App\Filament\Resources\PopularSearchResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreatePopularSearch extends CreateRecord
+{
+    protected static string $resource = PopularSearchResource::class;
+}
